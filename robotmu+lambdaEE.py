18393e5ccbc1fe068c4ddeac455bb66737d45ad0
MiquelEspinosa <mespinosami@gmail.com>
2020-11-13 10:03:19 +0100

    first working version

--- robotmu+lambdaEE.py
+++ robotmu+lambdaEE.py
@@ -3,95 +3,100 @@ import requests
 import time
 import matplotlib.pyplot as plt
 
-#TODO: Nos hemos quedado en la implementación correcta del cruze!!! 
-# Falta todo lo otro
-
-# Apuntes:
 # lamda = kids
 # mu = progenitores
 
-
 # CONSTANTES
-DNA_SIZE = 4                  # Corresponds to the number of motors
-DNA_BOUND = [-180, 180]       # Upper and lower bounds for DNA values
-POPULATION_SIZE = 10          # Population size
+DNA_SIZE = 4                             # Corresponds to the number of motors
+DNA_BOUND = [-180, 180]                  # Upper and lower bounds for DNA values
+POPULATION_SIZE = 100                     # Population size
 N_GENERATIONS = 100
-N_KID = 50                    # n kids per generation = lambda
-tau = 1/np.sqrt(DNA_SIZE)
+N_KID = 50                               # n kids per generation = lambda
+tau = 1/np.sqrt(2*np.sqrt(DNA_SIZE))     # consideramos b = 1
+tau0 = 1/np.sqrt(2*DNA_SIZE)             # consideramos b = 1
+size_tournament = 5
 
+# Plotting
+PLOTTING_REAL_TIME = 1  # Choose to show fitness plot in real time
+generations_plt = []    # Plotting axis
+fitness_curve = []      # Plotting curve
 
 # c1=3.412&c2=2.4&c3=15.42312&c4=-23.412235
 def evaluation(population):
     url_original = "http://163.117.164.219/age/robot4?"
-    fitness_population = np.empty([len(population['DNA']),DNA_SIZE])
+    fitness_population = np.empty(len(population['DNA']))
     for row, ind in enumerate(population['DNA']):
         url = url_original
         for rotor in range(0,len(ind)):
             # This is for getting the fitness of an specific individual
             url = url + str("c")+str(rotor+1)+str("=")+str(ind[rotor])+str("&")
         url = url[:-1]
-        for rotor in range(0,len(ind)):
-            try:
-                r = requests.get(url).content
-            except:
-                print("Exception when calling web service")
-                time.sleep(1)
-                r = requests.get(url).content
-            fitness_population[row][rotor] = float(r)
+        
+        try:
+            r = requests.get(url).content
+        except:
+            print("Exception when calling web service")
+            time.sleep(1)
+            r = requests.get(url).content
+        fitness_population[row] = float(r)
     return fitness_population
+
+def tournament(population, size_tournament, num_parents):
+    selected = []
+    for _ in range(num_parents):
+        winner = np.random.choice(np.arange(POPULATION_SIZE), size=size_tournament, replace=False)
+        selected.append(np.max(winner))
+    return selected
+    
 
 def make_kid(population, n_kid):
     # generate empty kid holder
     kids = {'DNA': np.empty((n_kid, DNA_SIZE))}
     kids['mut_strength'] = np.empty_like(kids['DNA'])
     
-    for kv, ks in zip(kids['DNA'], kids['mut_strength']):
-        # crossover (roughly half p1 and half p2)
-        # randomly choose 2 parents (indexes)
-        p1, p2 = np.random.choice(np.arange(POPULATION_SIZE), size=2, replace=False)
-        print(p1)
-        print(p2)
-        print(kv)
-        print(ks)
-
-        # cp = np.random.randint(0, 2, DNA_SIZE, dtype=np.bool)  # crossover points
-        # kv[cp] = population['DNA'][p1, cp]
-        # kv[~cp] = population['DNA'][p2, ~cp]
-        # ks[cp] = population['mut_strength'][p1, cp]
-        # ks[~cp] = population['mut_strength'][p2, ~cp]
+    for i in range(n_kid):
+        ks = np.empty(DNA_SIZE) # initialize
+        kv = np.empty(DNA_SIZE) # initialize
+        
+        parents = tournament(population['DNA'], size_tournament, 2)
+        p1 = parents[0]
+        p2 = parents[1]
 
         # uniform crossing with average among parents
         kv = (population['DNA'][p1] + population['DNA'][p2]) / 2
-        ks = np.sqrt(population['mut_strength'][p1] + population['mut_strength'][p2])
+        # cruce posicional de varianzas
+        cp = np.random.randint(0, 2, DNA_SIZE, dtype=np.bool)  # crossover points
+        ks[cp] = population['mut_strength'][p1, cp]
+        ks[~cp] = population['mut_strength'][p2, ~cp]
 
         # DNA mutation 
         kv = np.random.normal(kv,ks)
         # variances mutation 
-        ks = np.exp(np.multiply(np.random.normal(0,tau),ks))
-
+        ks = np.dot(ks, np.dot(np.exp(np.random.normal(0,tau,DNA_SIZE)), np.exp(np.random.normal(0,tau0,DNA_SIZE))))
+        
         # clip the mutated value
-        kv[:] = np.clip(kv, *DNA_BOUND)    
+        kv[:] = np.clip(kv, *DNA_BOUND)
 
-        # mutate (change DNA based on normal distribution)
-        # ks[:] = np.maximum(ks + (np.random.rand(*ks.shape)-0.5), 0.)    # must > 0
-        # kv += ks * np.random.randn(*kv.shape)
+        # update kids array
+        kids['DNA'][i] = kv
+        kids['mut_strength'][i] = ks
 
     return kids
 
-def get_fitness(pred): return pred.flatten()
+# def get_fitness(pred): return pred.flatten()
 
 def kill_bad(pop, kids):
     # put population and kids together
     for key in ['DNA', 'mut_strength']:
         pop[key] = np.vstack((pop[key], kids[key]))
 
-    fitness = get_fitness(evaluation(population))            # calculate global fitness
+    fitness = evaluation(population)            # calculate global fitness
     idx = np.arange(pop['DNA'].shape[0])
-    good_idx = idx[fitness.argsort()][-POP_SIZE:]   # selected by fitness ranking (not value)
+    index_sort = fitness.argsort()
+    good_idx = idx[index_sort][:POPULATION_SIZE]   # selected by fitness ranking (not value)
     for key in ['DNA', 'mut_strength']:
         pop[key] = pop[key][good_idx]
-    return pop
-
+    return pop, fitness[index_sort]
 
 
 # initialize randomly the population DNA values (motor angles)
@@ -99,27 +104,23 @@ def kill_bad(pop, kids):
 population = dict(DNA=np.random.uniform(low=DNA_BOUND[0], high=DNA_BOUND[1], size=(POPULATION_SIZE,DNA_SIZE) ),   
            mut_strength=np.random.rand(POPULATION_SIZE, DNA_SIZE)*DNA_BOUND[1])                                     
 
-print(population)
 
-for _ in range(N_GENERATIONS):
+if PLOTTING_REAL_TIME == 1:
+    plt.plot(generations_plt, fitness_curve, 'b', linewidth=1.0, label='Best individual fitness')
+    plt.xlabel('Generations')
+    plt.ylabel('Fitness')
+    plt.legend()
 
-    fitness_population = evaluation(population)
-    # print(fitness_population)
 
+for i in range(N_GENERATIONS):
 
     kids = make_kid(population, N_KID)
+    population, fitness_pop = kill_bad(population, kids)   # keep some good parent for elitism
+    # fitness_population = evaluation(population)
+    
+    generations_plt.append(i)
+    fitness_curve.append(fitness_pop[0])
 
-    print(kids)
-    population = kill_bad(population, kids)   # keep some good parent for elitism
-
-
-
-
-
-# Teacher comments:
-# P: en σ′=eN(0,τ)·σ, cuanto vale τ ?
-
-# R: es un parámetro que se llama tasa de aprendizaje
-#    debes jugar con el un poco
-#    debería de ser aprox 1/sqrt(n)
-#    siendo n la longitud del cromosoma
+    if PLOTTING_REAL_TIME == 1:
+            plt.plot(generations_plt, fitness_curve, 'b', linewidth=1.0, label='Best individual fitness')
+            plt.pause(0.05)
