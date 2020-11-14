b64d37d9319afaa596058557c55549d4d4b4d6f2
MiquelEspinosa <mespinosami@gmail.com>
2020-11-14 19:50:27 +0100

    good multithread. must use VPN and ethernet

--- robotmu+lambdaEE.py
+++ robotmu+lambdaEE.py
@@ -10,15 +10,15 @@ from threading import Thread
 
 # NETWORKING
 URL_PATH = "http://163.117.164.219/age/robot4?"
-N_THREADS = 10
-MAX_RETRIES = 20
+# N_THREADS = 10
+MAX_RETRIES = 10
 
 # CONSTANTES
 DNA_SIZE = 4                             # Corresponds to the number of motors
 DNA_BOUND = [-180, 180]                  # Upper and lower bounds for DNA values
-POPULATION_SIZE = 100                     # Population size
-N_GENERATIONS = 100
-N_KID = 200                               # n kids per generation = lambda
+POPULATION_SIZE = 200                     # Population size
+N_GENERATIONS = 1000
+N_KID = 250                               # n kids per generation = lambda
 tau = 1/np.sqrt(2*np.sqrt(DNA_SIZE))     # consideramos b = 1
 tau0 = 1/np.sqrt(2*DNA_SIZE)             # consideramos b = 1
 size_tournament = 5
@@ -27,9 +27,9 @@ size_tournament = 5
 PLOTTING_REAL_TIME = 1  # Choose to show fitness plot in real time
 generations_plt = []    # Plotting axis
 fitness_curve = []      # Plotting curve
+save_results = 'output'
 
 def process_individual(session, individual):
-    """ process a single individual """
     url = URL_PATH
     for rotor in range(0,len(individual)):
         url = url + str("c")+str(rotor+1)+str("=")+str(individual[rotor])+str("&")
@@ -42,81 +42,16 @@ def process_individual(session, individual):
         r = session.get(url).content
     return float(r)
 
-# def process_id_range(id_range, urls, session, store=None):
-#     """process a number of urls, storing the results in an array"""
-#     if store is None:
-#         store = {}
-#     for id in id_range:
-#         store[id] = process_individual(session, urls[id])
-#     return store
-
-# def get_urls(population):
-#     all_urls = []
-#     for ind in population['DNA']:
-#         url = URL_PATH
-#         for rotor in range(0,len(ind)):
-#             # This is for getting the fitness of an specific individual
-#             url = url + str("c")+str(rotor+1)+str("=")+str(ind[rotor])+str("&")
-#         url = url[:-1]
-#         all_urls.append(url)
-#     return all_urls
-
 def evaluation(population, session):
     pop_size = len(population['DNA'])
     population_fitness = np.empty(pop_size)
-    # urls = get_urls(population)
-
-    with futures.ThreadPoolExecutor(max_workers=100) as executor:
+    with futures.ThreadPoolExecutor(max_workers=POPULATION_SIZE) as executor:
         future = [
             executor.submit(process_individual, session, ind)
             for ind in population['DNA']
         ]
     population_fitness = [f.result() for f in future]
     return np.array(population_fitness)
-
-# def evaluation_bad(nthreads, population, session):
-#     """process the population in a specified number of threads"""
-#     id_range = range(len(population['DNA']))
-#     fitnesses = {}
-#     threads = []
-#     urls = get_urls(population)
-#     # create the threads
-#     for i in range(nthreads):
-#         ids = id_range[i::nthreads]
-#         t = Thread(target=process_id_range, args=(ids, urls, session, fitnesses))
-#         threads.append(t)
-    
-#     # start the threads
-#     [ t.start() for t in threads ]
-#     # wait for the threads to finish
-#     [ t.join() for t in threads ]
-    
-#     print("yes, joined")
-#     array = np.empty(len(population['DNA']))
-#     for i in range(len(population['DNA'])):
-#         array[i]=fitnesses[i]
-    
-#     return array
-
-
-# c1=3.412&c2=2.4&c3=15.42312&c4=-23.412235
-# def good_evaluation(population, session):
-#     fitness_population = np.empty(len(population['DNA']))
-#     for row, ind in enumerate(population['DNA']):
-#         url = URL_PATH
-#         for rotor in range(0,len(ind)):
-#             # This is for getting the fitness of an specific individual
-#             url = url + str("c")+str(rotor+1)+str("=")+str(ind[rotor])+str("&")
-#         url = url[:-1]
-
-#         try:
-#             r = session.get(url).content
-#         except:
-#             print("Exception when calling web service")
-#             time.sleep(1)
-#             r = session.get(url).content
-#         fitness_population[row] = float(r)
-#     return fitness_population
 
 def tournament(population, size_tournament, num_parents):
     selected = []
@@ -125,16 +60,14 @@ def tournament(population, size_tournament, num_parents):
         selected.append(np.max(winner))
     return selected
     
-
 def make_kid(population, n_kid):
     # generate empty kid holder
     kids = {'DNA': np.empty((n_kid, DNA_SIZE))}
     kids['mut_strength'] = np.empty_like(kids['DNA'])
-    
+
     for i in range(n_kid):
-        ks = np.empty(DNA_SIZE) # initialize
-        kv = np.empty(DNA_SIZE) # initialize
-        
+        ks = np.empty(DNA_SIZE) # initialize array
+        kv = np.empty(DNA_SIZE) # initialize array        
         parents = tournament(population['DNA'], size_tournament, 2)
         p1 = parents[0]
         p2 = parents[1]
@@ -146,9 +79,8 @@ def make_kid(population, n_kid):
         ks[cp] = population['mut_strength'][p1, cp]
         ks[~cp] = population['mut_strength'][p2, ~cp]
 
-        # DNA mutation 
+        # DNA and variances mutation 
         kv = np.random.normal(kv,ks)
-        # variances mutation 
         ks = np.dot(ks, np.dot(np.exp(np.random.normal(0,tau,DNA_SIZE)), np.exp(np.random.normal(0,tau0,DNA_SIZE))))
         
         # clip the mutated value
@@ -160,7 +92,6 @@ def make_kid(population, n_kid):
 
     return kids
 
-# def get_fitness(pred): return pred.flatten()
 
 def kill_bad(pop, kids, session):
     # put population and kids together
@@ -173,14 +104,30 @@ def kill_bad(pop, kids, session):
     good_idx = idx[index_sort][:POPULATION_SIZE]   # selected by fitness ranking (not value)
     for key in ['DNA', 'mut_strength']:
         pop[key] = pop[key][good_idx]
-    return pop, fitness[index_sort]
+    return pop, fitness[good_idx]
+
+def write_header_txt(file):
+    file.write('------------------------------------ \n')
+    file.write('PARAMETERS used: \n')
+    file.write(' - POPULATION SIZE (parents): %r\n' % str(POPULATION_SIZE))
+    file.write(' - NUMBER OF KIDS: %r\n' % str(N_KID))
+    file.write(' - SIZE TOURNAMENT: %r\n' % str(size_tournament))
+    file.write(' - DNA SIZE: %r\n' % str(DNA_SIZE))
+    file.write('------------------------------------ \n\n\n')
+    file.write('------------------------------------ \n')
+    file.write('Fitness_value\t\tIndividual\n')
+    file.write('------------------------------------ \n')
 
 def main():
+    name = str(save_results+'.txt')
+    file = open(name, "w")
+    write_header_txt(file)
+
     # initialize randomly the population DNA values (motor angles)
     # initialize randomly the variances with big values
     population = dict(DNA=np.random.uniform(low=DNA_BOUND[0], high=DNA_BOUND[1], size=(POPULATION_SIZE,DNA_SIZE) ),   
-            mut_strength=np.random.rand(POPULATION_SIZE, DNA_SIZE)*DNA_BOUND[1])                                     
-
+            mut_strength=np.random.rand(POPULATION_SIZE, DNA_SIZE)*(DNA_BOUND[1]/2))                                     
+    
     # for plotting
     if PLOTTING_REAL_TIME == 1:
         plt.plot(generations_plt, fitness_curve, 'b', linewidth=1.0, label='Best individual fitness')
@@ -193,19 +140,33 @@ def main():
     adapter = requests.adapters.HTTPAdapter(pool_connections=POPULATION_SIZE, pool_maxsize=POPULATION_SIZE, max_retries=MAX_RETRIES)
     session.mount('http://', adapter)
 
-
     for i in range(N_GENERATIONS):
-        # print(population['mut_strength'])
-
         kids = make_kid(population, N_KID)
         population, fitness_pop = kill_bad(population, kids, session)   # keep some good parent for elitism
         # fitness_population = evaluation(population)
         
+        print(population['mut_strength'][0])
+
+        file.write(' %r\t\t\t%r\n\t\t\t\t\t\t%r\n\n' % (fitness_pop[0],population['DNA'][0],population['mut_strength'][0]))
+
         if PLOTTING_REAL_TIME == 1:
             generations_plt.append(i)
             fitness_curve.append(fitness_pop[0])
             plt.plot(generations_plt, fitness_curve, 'b', linewidth=1.0, label='Best individual fitness')
             plt.pause(0.05)
+
+        if (fitness_pop[0] == 0 or i == N_GENERATIONS-1):
+            plt.savefig(str(save_results+'.png'))
+            file.write('------------------------------------ \n\n\n')
+            file.write('------------------------------------ \n')
+            file.write('TOTAL GENERATIONS: %r\n' % (i+1))
+            file.write('------------------------------------ \n')
+            file.close()
+            if fitness_pop[0] == 0:
+                print(" => Optimal solution has been found!")
+            else:
+                print(" *** Maximum number of iterations reached ***")
+            break
 
 
 if __name__ == "__main__":
